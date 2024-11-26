1847f93b404095c0cbf9c22280648b648b917496
Evan Hahn <me@evanhahn.com>
2024-11-26 20:11:19 +0000

    feat: support more geometry types
    In addition to `Point`, we now support additional geometry objects.
    
    Closes [#35].
    
    [#35]: https://github.com/digidem/comapeo-cloud/issues/35

--- test/add-alerts-endpoint.js
+++ test/add-alerts-endpoint.js
@@ -18,7 +18,6 @@ import {
   runWithRetries,
 } from './test-helpers.js'
 
-/** @import { RemoteDetectionAlertValue } from '@comapeo/schema'*/
 /** @import { FastifyInstance } from 'fastify' */
 
 test('returns a 401 if no auth is provided', async (t) => {
@@ -119,16 +118,6 @@ test('returns a 400 if trying to add invalid alerts', async (t) => {
         coordinates: [0, 90.01],
       },
     },
-    {
-      ...generateAlert(),
-      geometry: {
-        type: 'MultiPoint',
-        coordinates: [
-          [1, 2],
-          [3, 4],
-        ],
-      },
-    },
     ...alertKeys.flatMap((keyToMessUp) => [
       omit(generateAlert(), keyToMessUp),
       { ...generateAlert(), [keyToMessUp]: null },
@@ -177,32 +166,42 @@ test('adding alerts', async (t) => {
     dangerouslyAllowInsecureConnections: true,
   })
 
-  const alert = generateAlert()
+  const alerts = generateAlerts(100)
 
-  const response = await server.inject({
-    authority: serverUrl.host,
-    method: 'POST',
-    url: `/projects/${projectId}/remoteDetectionAlerts`,
-    headers: {
-      Authorization: 'Bearer ' + BEARER_TOKEN,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(alert),
-  })
-  assert.equal(response.statusCode, 201)
-  assert.equal(response.body, '')
+  await Promise.all(
+    alerts.map(async (alert) => {
+      const response = await server.inject({
+        authority: serverUrl.host,
+        method: 'POST',
+        url: `/projects/${projectId}/remoteDetectionAlerts`,
+        headers: {
+          Authorization: 'Bearer ' + BEARER_TOKEN,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(alert),
+      })
+      assert.equal(response.statusCode, 201)
+      assert.equal(response.body, '')
+    }),
+  )
 
   project.$sync.start()
   project.$sync.connectServers()
 
   await project.$sync.waitForSync('full')
 
+  const expectedSourceIds = new Set(alerts.map((a) => a.sourceId))
+
   // It's possible that the client thinks it's synced but doesn't know about
   // the server's alert yet, so we try a few times.
   await runWithRetries(3, async () => {
     const alerts = await project.remoteDetectionAlert.getMany()
-    const hasOurAlert = alerts.some((a) => a.sourceId === alert.sourceId)
-    assert(hasOurAlert, 'alert was added and synced')
+    const actualSourceIds = new Set(alerts.map((a) => a.sourceId))
+    assert.deepEqual(
+      actualSourceIds,
+      expectedSourceIds,
+      'alerts were added and synced',
+    )
   })
 })
 
@@ -223,23 +222,17 @@ async function addProject(server) {
   return projectKeyToPublicId(Buffer.from(projectKey, 'hex'))
 }
 
-/**
- * @param {number} min
- * @param {number} max
- * @returns {number}
- */
-const randomNumber = (min, max) => min + Math.random() * (max - min)
-const randomLatitude = randomNumber.bind(null, -90, 90)
-const randomLongitude = randomNumber.bind(null, -180, 180)
-
 function generateAlert() {
-  const remoteDetectionAlertDoc = generate('remoteDetectionAlert')[0]
-  assert(remoteDetectionAlertDoc)
-  return valueOf({
-    ...remoteDetectionAlertDoc,
-    geometry: {
-      type: 'Point',
-      coordinates: [randomLongitude(), randomLatitude()],
-    },
+  const [result] = generateAlerts(1)
+  assert(result)
+  return result
+}
+
+/**
+ * @param {number} count
+ */
+function generateAlerts(count) {
+  return generate('remoteDetectionAlert', { count }).map((alert) => {
+    return valueOf(alert)
   })
 }
